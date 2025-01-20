d19980b093fbb123054cb6e807bee760da9482bb
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2025-01-20 16:52:09 +0700

    style: format code for consistency and readability

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,22 +1,22 @@
-import { getDefaultQuestionSetList } from '@/lib/action/questionSet.action';
-import { IQuestionSet } from '@/lib/database/model/questionSet.model';
-import { IQuestionSetMeta } from '@/lib/database/model/questionSetMeta.model';
-import React from 'react';
+import { getDefaultQuestionSetList } from "@/lib/action/questionSet.action";
+import { IQuestionSet } from "@/lib/database/model/questionSet.model";
+import { IQuestionSetMeta } from "@/lib/database/model/questionSetMeta.model";
+import React from "react";
 
 async function Page() {
-    const questionSet: IQuestionSetMeta[] = await getDefaultQuestionSetList();
+  const questionSet: IQuestionSetMeta[] = await getDefaultQuestionSetList();
 
-    return (
-        <main>
-            <h1>Quiz</h1>
-            {questionSet.map((set, index) => (
-                <div key={index}>
-                    <h2>{set.title}</h2>
-                    <p>{set.description}</p>
-                </div>
-            ))}
-        </main>
-    )
+  return (
+    <main>
+      <h1>Quiz</h1>
+      {questionSet.map((set, index) => (
+        <div key={index}>
+          <h2>{set.title}</h2>
+          <p>{set.description}</p>
+        </div>
+      ))}
+    </main>
+  );
 }
 
-export default Page
+export default Page;
